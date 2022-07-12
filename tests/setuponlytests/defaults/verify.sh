3aa65723d89866dd48404e2a7c556bc4d1020a33
Shreyansh sheth <shethshreyansh777@gmail.com>
2022-07-13 00:31:16 +0530

    changes

--- tests/setuponlytests/defaults/verify.sh
+++
@@ -1,3 +0,0 @@
-mc-image-helper assert propertyEquals --file=server.properties --property=rcon.password --expect=minecraft
-mc-image-helper assert propertyEquals --file=server.properties --property=rcon.port --expect=25575
-mc-image-helper assert propertyEquals --file=server.properties --property=enable-rcon --expect=true
